ed5540dc6c91c9f9ae13ebc2e4d456a88ad019b9
Aric Hylton <Arich212@gmail.com>
2025-02-06 10:04:07 -0600

    updated packages

--- linear-regression/main.py
+++ linear-regression/main.py
@@ -1,3 +1,19 @@
 import numpy as np
+import pandas as pd 
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+import statsmodels.api as sm
+
+data = pd.read_csv('./Simple_linear_regression.csv') 
+
+y = data['GPA']
+x1 = data['SAT']
+
+plt.scatter(x1, y)
+plt.xlabel('SAT', fontsize=20)
+plt.ylabel('GPA', fontsize=20)
+plt.show()
 
 
